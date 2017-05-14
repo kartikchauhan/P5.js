be129d80f8caddebfdec87009678286af08d35c2
kartik chauhan <chauhan.kartik25@gmail.com>
2017-05-14 06:02:06 +0530

    change color of bubbles on collision

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,9 +1,10 @@
 var bubble = [];
+var numBubbles = 10;
 
 function setup()
 {
     createCanvas(500, 500);
-    for(var i=0; i<10; i++)
+    for(var i=0; i<numBubbles; i++)
     {
         bubble[i] = new Bubble();
     }
@@ -12,46 +13,52 @@ function setup()
 function draw()
 {
     background(0);
-    for(var i=0; i<10; i++)
+    for(var i=0; i<numBubbles; i++)
     {
         bubble[i].display();
         bubble[i].move();
+        for(var j=0; j<numBubbles; j++)
+        {
+            if(i != j)
+            {
+                if(bubble[i].collide(bubble[j]))
+                {
+                    bubble[i].changeColor();
+                    bubble[j].changeColor();
+                }
+            }
+        }
     }
     
 }
 
-function mousePressed()
+function Bubble()
 {
-    for(var i=0; i<10; i++)
-    {
-        bubble[i].clicked();
-    }
-}
-
-function Bubble()   // constructor function
-{
-    var majorAxis = 50, minorAxis = 50; // majorAxis and minorAxis of the ellipse, in this case it'll be circle with diameter 50
-
     this.x = random(0, width);
     this.y = random(0, height);
+    this.radius = 25;
     this.col = color(255);
-    
-    this.clicked = function() {
-        var distance = dist(mouseX, mouseY, this.x, this.y);
-        if(distance < majorAxis)
-            {
-                this.col = color(255, 100, 0);  // change color to orange when a circle is clicked
-            }
-    }
     
     this.display = function() {
         fill(this.col);
-        ellipse(this.x, this.y, majorAxis, majorAxis);
+        ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
     }
     
     this.move = function() {
         this.x = this.x + random(-5, 5);
         this.y = this.y + random(-5, 5);
+    }
+    
+    this.changeColor = function() {
+        this.col = color(random(255), random(255), random(255));
+    }
+    
+    this.collide = function(otherObj) {
+        var dis = dist(this.x, this.y, otherObj.x, otherObj.y);
+        if(dis <= (this.radius + otherObj.radius))
+            return true;
+        else
+            return false;
     }
     
 }
